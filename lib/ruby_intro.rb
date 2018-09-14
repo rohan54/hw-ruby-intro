21cb7ab7baff37cc3771a44ea944cc05d95425c6
EC2 Default User <ec2-user@ip-172-31-40-159.us-east-2.compute.internal>
2018-09-14 04:45:49 +0000

    HW1-Ruby Intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,69 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  max2 = arr.max(2) # Extracting max 2 numbers from array
+  max2.inject(0, :+)# Computing sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr2 = arr.combination(2) #Creates every possible combination of 2 elements
+  ans = false
+  #Iterate over each combination to check its equality to n
+  arr2.each do |arr1| 
+              sum = arr1.inject(0,:+)
+              if sum == n
+                ans = true 
+               break
+              end
+            end
+  ans
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, "+ name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  #If starting character is not a,e,i,o,u or non-letter then it will be a consonant.
+  #not a,e,i,o,u or non-letter => ^aeiou\W
+  #^ means not
+  #\W means non-letter
+  #Abve condition is for first character hence ^[....]
+  #i at the end makes match case insensitive
+  
+  /^[^aeiou\W]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  #For binary multiple of 4, last 2 bits should be 00 or string can itself be 0
+  /[^01]/.match(s)==nil && ( s.end_with?("00") ) || s=="0"
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  
+  attr_accessor :isbn, :price
+  
+  def initialize(isbn, price) 
+    if(isbn=="" || price<=0)
+      raise ArgumentError
+    end 
+    # Instance variables  
+    @isbn = isbn  
+    @price = price  
+  end  
+  
+  def price_as_string
+    #Restrict float to 2 decimals
+    num = '%.2f' % @price
+    "$"+"#{num}"
+  end
+  
 end
